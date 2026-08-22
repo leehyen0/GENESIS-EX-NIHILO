fcd09034a228a55c2f637e8cc0451eca0dfc9f3f
leehyen0 <yesancom08@gmail.com>
2026-08-22 14:50:12 +0900

    Test G4 against complete G3 prediction surface

--- arte_cognition/test_sparse_minterm_genesis.py
+++ arte_cognition/test_sparse_minterm_genesis.py
@@ -8,12 +8,16 @@ from arte_cognition.sparse_minterm_genesis import SparseMintermCausalGenesisEngi
 
 
 class SparseMintermCausalGenesisTests(unittest.TestCase):
-    def test_generation_four_contains_three_minterm_signatures_absent_from_g3(self):
+    def test_generation_four_contains_three_minterm_signatures_absent_from_complete_g3(self):
         variables = ["x", "z"]
         descriptors = InterventionSurfaceGenesisEngine(budget=256).generate(variables)
-        g3_engine = BooleanCausalPredicateGenesisEngine(model_budget=2048, max_literals_per_term=3, max_terms=2)
+        g3_engine = BooleanCausalPredicateGenesisEngine(
+            model_budget=16384, max_literals_per_term=3, max_terms=2
+        )
         g3 = g3_engine.generate_novel(variables, descriptors, (), ())
         self.assertFalse(g3_engine.last_truncated)
+        self.assertGreater(g3_engine.last_unique_signature_count, 2048)
+
         g4_engine = SparseMintermCausalGenesisEngine(model_budget=4096, max_minterms=3)
         g4 = g4_engine.generate_novel(variables, descriptors, (), [item.model for item in g3])
         self.assertFalse(g4_engine.last_truncated)
